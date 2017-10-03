25d9afe590c9e4aa224dd336e6b78769823efbce
Elvis Hang <elvishang@Elviss-MacBook-Pro.local>
2017-10-03 17:27:35 -0500

    completed object_challenge assignment

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 var locations = []; // this array will hold your objects
 
-// object constructor
+var Region = function (state, revenue, locationCount){
+  this.state = state;
+  this.revenue = revenue;
+  this.locationCount = locationCount;
+  this.averageRevenue = function (){
+    return this.revenue / this.locationCount;
+  };
+};// object constructor
 
+var regionIL = new Region("IL", 5000, 12);
+var regionMN = new Region("MN", 300, 3);
+var regionNV = new Region("NV", 25000, 1);
 // object instances
 
-// push object instances to locations array
+locations.push(regionIL, regionMN, regionNV);// push object instances to locations array
 
 // DO NOT MODIFY THIS CODE
 $(document).ready(function () {
